70f7609a5435aa755802aef17807bbb70f08b58a
ivanovdale <dania-spb@mail.ru>
2023-12-11 11:33:11 +0300

    Add project files

---
+++ mb_ios/mb_ios/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  mb_ios
+//
+//  Created by Daniil (work) on 11.12.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
